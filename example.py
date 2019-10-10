57bad951dd96e6549c3a122c820c05fb07180dbe
John Lingi <lingijohn23@gmail.com>
2019-10-10 23:42:10 +0100

    merged with v2

--- example.py
+++
@@ -1,9 +0,0 @@
-from clippings import Clippings
-
-if __name__ == "__main__":
-    obj = Clippings()
-    obj.load_from_text("test.txt")
-
-    print(obj.get_booklist())
-
-    obj.write_to_file(book="book1", file="saved_text.txt")
